43f2e3a8df69249ca7d2e1a2deb7925376f577ca
Daniolo <dani_tarrassotoscano@hotmail.com>
2018-10-30 20:13:57 +0100

    Initial Commit

---
+++ MoneyConverter/Conversion.swift
@@ -0,0 +1,15 @@
+//
+//  Conversion.swift
+//  MoneyConverter
+//
+//  Created by Daniel Tarrasso on 30/10/18.
+//  Copyright © 2018 Daniel Tarrasso. All rights reserved.
+//
+
+import Foundation
+
+public class Conversion{
+    var input: Int
+    var 
+}
+
---
+++ MoneyConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MoneyConverter
+//
+//  Created by Daniel Tarrasso on 30/10/18.
+//  Copyright © 2018 Daniel Tarrasso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    class UIImageView : UIView{}
+
+}
+
