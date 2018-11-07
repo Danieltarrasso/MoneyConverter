bcce55a3bcd0c5ec9393ca359ddd70d7ce221c7f
Daniolo <dani_tarrassotoscano@hotmail.com>
2018-11-07 17:05:13 +0100

    picker2.0

--- MoneyConverter/ViewController.swift
+++ MoneyConverter/ViewController.swift
@@ -9,33 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var arrayCoin: [Moneda] = []
+    
+    var name = ["euro", "dollar", "peso"]
+    var value = ["1.1409", "0.876521", "0,044"]
+    var flag = [#imageLiteral(resourceName: "spainflag"), #imageLiteral(resourceName: "USA-PNG-Clipart"), #imageLiteral(resourceName: "1280px-Flag_of_Mexico.svg")]
+    var background = [#imageLiteral(resourceName: "barcelona"), #imageLiteral(resourceName: "usa"), #imageLiteral(resourceName: "mexico")]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent: Int) -> Int {
+        return name.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) ->String?{
+        return name[row]
+    }
      
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        var arrayCoin: [Moneda] = []
-        
-        var name = ["euro", "dollar", "peso"]
-        var value = ["1.1409", "0.876521", "0,044"]
-        var flag = [#imageLiteral(resourceName: "spainflag"), #imageLiteral(resourceName: "USA-PNG-Clipart"), #imageLiteral(resourceName: "1280px-Flag_of_Mexico.svg")]
-        var background = [#imageLiteral(resourceName: "barcelona"), #imageLiteral(resourceName: "usa"), #imageLiteral(resourceName: "mexico")]
-        
+   
         
         for c in name{
             arrayCoin.append(Moneda(name: name[c], value: value[c], flag: flag[c], background: background[c]))
         }
-        
-        pickerView.delegate = self
-        pickerView.dataSource = self
     }
     
     
-    
-    @IBOutlet weak var background: UIImageView!
-    @IBOutlet weak var flag: UIImageView!
-    @IBOutlet var backgoundImage: UIView!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -43,7 +48,6 @@ class ViewController: UIViewController {
     
     
     @IBAction func btn_next(_ sender: Any) {
-        arrayCoin = UIImage(named: "usa.png")
     }
     
     @IBAction func btn_previous(_ sender: Any) {
